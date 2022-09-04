13ff391ba20306164da85988e851a8f30a6617d3
Osman Tigci <osmantigcii@gmail.com>
2022-09-04 14:47:08 +0300

    patika.dev

--- patika.dev - Projeler/HackerRankFirst.java
+++ patika.dev - Projeler/HackerRankFirst.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
+public class HackerRankFirst{
+    public static void main(String []argh) {
 
-public class HackerRankFirst {
-    public static void main(String[] args) {
 
-        Scanner inp = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
 
-        System.out.print("int değerini giriniz: ");
-        int T = inp.nextInt();
+        for(int i=0;i<t;i++)
+        {
 
-        String t1 = (T >= -128)&&(T <= 127) ? "T can be fitted in byte" : "T can't be fitted in byte";
-        String t2 = (T <= 32.767)&&(T >= -32.768) ? "T can be fitted in short" : "T can't be fitted in short";
-        String t3 = (T >= Math.pow(-2,31))&&(T <= Math.pow(2,31) -1) ? "T can be fitted in int" : "T can't be fitted in int";
-        String t4 = (T <= Math.pow(2,63) - 1)&&(T >= Math.pow(-2, 63)) ? "T can be fitted in long" : "T can't be fitted anywhere.";
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+                if(x>= Math.pow(-2,31)&& x<= Math.pow(2,31) -1)System.out.println("* int");
+                if(x>= Math.pow(-2,63)&& x<= Math.pow(2,63) -1)System.out.println("* long");
+                //Complete the code
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
 
-        System.out.println(t1);
-        System.out.println(t2);
-        System.out.println(t3);
-        System.out.println(t4);
-
+        }
     }
 }
