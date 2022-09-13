ab606cd36aad3dcf30a1cadc76eaa69946928678
Osman Tigci <osmantigcii@gmail.com>
2022-09-14 00:40:16 +0300

    patika.dev

---
+++ patika.dev - Projeler/HesapMakinesi.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class HesapMakinesi {
+    public static void main(String[] args) {
+        double n1;
+        int n2, select;
+        Scanner input = new Scanner (System.in);
+        System.out.print("İlk Sayıyı Giriniz : ");
+        n1 = input.nextDouble();
+        System.out.print("İkinci Sayıyı Giriniz : ");
+        n2 = input.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz : ");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("Toplam :" + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Çıkarma :" + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Çarpma :" + (n1 * n2));
+                break;
+            case 4:
+                switch (n2) {
+                    case 0:
+                        System.out.println("Sayı sıfıra bölünemez.");
+                        break;
+                    default :
+                        System.out.println("Bölme :" + (n1 / n2));
+                        break;
+                }
+                break;
+            default:
+                System.out.println("Geçersiz işlem yaptınız. Tekrar deneyiniz.");
+        }
+    }
+}
